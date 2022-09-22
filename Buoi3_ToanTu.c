777581eca98c3755aad2c60297fdb3bbc2dcd435
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2022-09-22 18:21:09 +0700

    Buổi 3: Toán tử

---
+++ Buoi3_ToanTu.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    // printf("Hello World");
+    // int  number = 5;
+    // printf("Number before= %d\n",number);
+    // printf("Vui long nhap so n:");
+    // scanf("%d",&number);
+    // printf("Number after: %d",number);
+    
+    // Có 4 loại toán tử 
+    // Toán tử số học: +,-,*,/,%
+    // Toán tử so sánh: >,<,>=,<=,== 
+    // Toán tử logic: &&(AND), ||(OR), !(NOT)
+    // Toán tử gán: =,+=,-=,....
+
+    return 0;
+}
