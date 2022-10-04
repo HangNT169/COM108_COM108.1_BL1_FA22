a3061529f71c9f2f09e7c1309157feafa3e9c13a
HangNT169 <104345338+HangNT169@users.noreply.github.com>
2022-10-04 17:39:36 +0700

    Update

---
+++ Buoi8_VongLapP1.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    // while 
+    // while(dieu kien){
+    //     // cong viec muon lap di lap lai
+    // }
+    // int i = 1;
+    // while(i<11){
+    //     printf("%d\n",i);
+    //     i=i+1;
+    // }
+    
+    // B1: In cac so tu 0 toi 20
+    // B2: Tinh tong cac so tu 0 toi 20 
+    // B3: Tinh tong cac so chan tu 0 toi 10 
+    // B4: Dem cac so chan tu 0 toi 10 
+    int i = 0;
+    // int sum = 0;
+    // while(i<11){
+    //     // // printf("%d\n",i);
+    //     // // tinh tong 
+    //     // sum = sum+i;
+    //     // // sum+=i 
+    //     // i=i+1;
+    //     // // i++ // khoang cach la 1. 
+    //     // // i+=1 
+    //     // // i=i+2 
+    //     // // i+=2 
+    //     if(i%2==0){
+    //         sum+=i;
+    //     }
+    //     i++;
+    // }
+    // do...while 
+    // do{
+        // code
+    // }while(dieuKien)
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i<6);
+    // printf("Sum = %d\n",sum);
+    return 0;
+}
