de226417db96e398a2dec9ade7da177d28a2415c
HangNT169 <HangNT169@fpt.edu.vn>
2023-05-03 12:15:39 +0700

    aa

---
+++ a.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    /**
+     * Tat cac cac dong lenh ket t huc bang dau ;
+     * printf: Dung de in bat cu thong tin mong muon
+     * \n: Dung de xuong dong
+     * Co 2 cach comment: Khi chuong trinh chay nhung dong, cau lenh , khoi lenh comment se khong duoc hien thi 
+     *    c1: An Ctrl /
+     *    c2: 
+     *  Cac loai kieu du lieu :
+     *  - so nguyen : int/long
+     *  - so thuc : float / double 
+     *  - ki tu: char 
+     *  - true/false: bool
+     *  Cach khai bao bien : kieu du lieu tenBien
+     * VD: int a; 
+     *     float b;
+     *     bool c;
+     */
+
+    printf("hihihi\n");
+    // printf("Hello world");
+    return 0;
+}
